b926ffef6e1a45aef920d1bf6f78a42db453b25e
Adriana Mendes <adrianamirianmc@gmail.com>
2022-06-30 23:37:21 -0300

    ci: Ajuste backend Heroku

--- spring/nosso-cafe/src/main/java/com/spring/api/config/CorsConfiguration.java
+++ spring/nosso-cafe/src/main/java/com/spring/api/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE",
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://unidac-nosso-cafe-frontend.herokuapp.com").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE",
 				"CONNECT");
 	}
 }
